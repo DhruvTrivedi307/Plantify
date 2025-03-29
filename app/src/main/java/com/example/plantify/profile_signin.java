0d23cbefd04680c08158e4fcd90324df083a2954
Jatin-Tahelyani <141558170+Jatin-Tahelyani@users.noreply.github.com>
2025-03-29 13:57:44 +0530

    29 march 1:53

---
+++ app/src/main/java/com/example/plantify/profile_signin.java
@@ -0,0 +1,42 @@
+package com.example.plantify;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class profile_signin extends AppCompatActivity {
+
+    BottomNavigationView bnv;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_profile_signin);
+
+        bnv = findViewById(R.id.bnv);
+
+        bnv.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId()==R.id.home){
+                    Intent i = new Intent(profile_signin.this,home.class);
+                    startActivity(i);
+                    return true;
+                }
+                return true;
+            }
+        });
+    }
+}
