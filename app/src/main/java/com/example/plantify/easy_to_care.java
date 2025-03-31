e2f9096b5fe04b5fec3641566ffb9d96a5d42c90
DhruvTrivedi307 <dhruvmtrivedi07@gmail.com>
2025-03-31 17:30:31 +0530

    Updated 31 mar 04:34 am

--- app/src/main/java/com/example/plantify/easy_to_care.java
+++ app/src/main/java/com/example/plantify/easy_to_care.java
@@ -1,24 +1,173 @@
 package com.example.plantify;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class easy_to_care extends AppCompatActivity {
 
+    LinearLayout e_apbp, e_bpp, e_bpx, e_arp, e_flp, e_csp, e_mdp, e_pgp, e_vjmp, e_bhp;
+    AppCompatButton e_apbp_cart_click, e_bpp_cart_click, e_bpx_cart_click, e_arp_cart_click, e_flp_cart_click, e_csp_cart_click, e_mdp_cart_click, e_pgp_cart_click, e_vjmp_cart_click, e_bhp_cart_click;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_easy_to_care);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        e_apbp = findViewById(R.id.e_apbp);
+        e_bpp = findViewById(R.id.e_bpp);
+        e_bpx = findViewById(R.id.e_bpx);
+        e_arp = findViewById(R.id.e_arp);
+        e_flp = findViewById(R.id.e_flp);
+        e_csp = findViewById(R.id.e_csp);
+        e_mdp = findViewById(R.id.e_mdp);
+        e_pgp = findViewById(R.id.e_pgp);
+        e_vjmp = findViewById(R.id.e_vjmp);
+        e_bhp = findViewById(R.id.e_bhp);
+        e_apbp_cart_click = findViewById(R.id.e_apbp_cart_click);
+        e_bpp_cart_click = findViewById(R.id.e_bpp_cart_click);
+        e_bpx_cart_click = findViewById(R.id.e_bpx_cart_click);
+        e_arp_cart_click = findViewById(R.id.e_arp_cart_click);
+        e_flp_cart_click = findViewById(R.id.e_flp_cart_click);
+        e_csp_cart_click = findViewById(R.id.e_csp_cart_click);
+        e_mdp_cart_click = findViewById(R.id.e_mdp_cart_click);
+        e_pgp_cart_click = findViewById(R.id.e_pgp_cart_click);
+        e_vjmp_cart_click = findViewById(R.id.e_vjmp_cart_click);
+        e_bhp_cart_click = findViewById(R.id.e_bhp_cart_click);
+
+        e_apbp.setOnClickListener(v -> {
+            String name = "Aglaonema Pink Beauty Plant";
+            int price = 499;
+            String description = "This variety of Aglaonema, often referred to as ‘Pink Aglaonema’ or ‘Lady Valentine,’ is renowned for its striking pink and green variegated leaves. It’s a popular houseplant due to its vibrant foliage and relatively easy care requirements.  ";
+            RedirectProduct(R.drawable.aglaonema_pink_beauty_plant,name,price,description);
         });
+
+        e_bpp.setOnClickListener(v -> {
+            String name = "Bamboo Plam Plant";
+            int price = 499;
+            String description = "Known for its elegant, feathery fronds, the Bamboo Palm (Chamaedorea seifrizii) is a popular indoor plant that thrives in low light conditions and helps purify indoor air.";
+            RedirectProduct(R.drawable.bamboo_palm_plant,name,price,description);
+        });
+
+        e_bpx.setOnClickListener(v -> {
+            String name = "Bamboo Palm XL";
+            int price = 499;
+            String description = "This is likely a larger variant of the Bamboo Palm, offering the same air-purifying benefits and aesthetic appeal as its standard counterpart.";
+            RedirectProduct(R.drawable.bamboo_palm_xl,name,price,description);
+        });
+
+        e_arp.setOnClickListener(v -> {
+            String name = "Aglaonema Red Planin";
+            int price = 499;
+            String description = "Similar to the Pink Beauty, the Red Aglaonema features lush foliage with red and green hues, adding a vibrant touch to indoor spaces.";
+            RedirectProduct(R.drawable.aglaonema_red_plant,name,price,description);
+        });
+
+        e_flp.setOnClickListener(v -> {
+            String name = "Fiddle Leaf Fig Plant";
+            int price = 499;
+            String description = "Recognized for its large, glossy leaves, the Fiddle Leaf Fig (Ficus lyrata) is a popular choice for adding a bold, green statement to interiors.  ";
+            RedirectProduct(R.drawable.fiddle_leaf_fig_plant,name,price,description);
+        });
+
+        e_csp.setOnClickListener(v -> {
+            String name = "Chlorophytum Spider Plant";
+            int price = 499;
+            String description = "The Spider Plant (Chlorophytum comosum) is an easy-to-grow houseplant known for its arching leaves and small white flowers. It’s also appreciated for its air-purifying qualities.";
+            RedirectProduct(R.drawable.chlorophytum_spider_plant,name,price,description);
+        });
+
+        e_mdp.setOnClickListener(v -> {
+            String name = "Monstera Deliciosa Plant";
+            int price = 499;
+            String description = "Known for its unique, glossy, and large leaves with natural splits and holes, the Monstera Deliciosa adds a tropical vibe to any space. It’s a low-maintenance plant that thrives in indirect light and requires minimal watering.  ";
+            RedirectProduct(R.drawable.monstera_deliciosa_plant,name,price,description);
+        });
+
+        e_pgp.setOnClickListener(v -> {
+            String name = "Peperomia Green Plant";
+            int price = 499;
+            String description = "Peperomia is a compact, low-maintenance plant with attractive foliage, making it ideal for small spaces or desktops.";
+            RedirectProduct(R.drawable.peperomia_green_plant,name,price,description);
+        });
+
+        e_vjmp.setOnClickListener(v -> {
+            String name = "Variegated Jade Mini Plant";
+            int price = 499;
+            String description = "A smaller variant of the Jade Plant (Crassula ovata) featuring variegated leaves. Jade plants are succulents known for their thick, fleshy leaves and are considered symbols of good luck.";
+            RedirectProduct(R.drawable.variegated_jade_mini_plant,name,price,description);
+        });
+
+        e_bhp.setOnClickListener(v -> {
+            String name = "Broken Heart Plant";
+            int price = 499;
+            String description = "Commonly known as the Swiss Cheese Plant, it boasts large, glossy leaves with unique holes, adding a tropical feel to indoor spaces.";
+            RedirectProduct(R.drawable.broken_heart_plant,name,price,description);
+        });
+
+        e_apbp_cart_click.setOnClickListener(v -> {
+            onCartClick();
+        });
+
+        e_bpp_cart_click.setOnClickListener(v -> {
+            onCartClick();
+        });
+
+        e_bpx_cart_click.setOnClickListener(v -> {
+            onCartClick();
+        });
+
+        e_arp_cart_click.setOnClickListener(v -> {
+            onCartClick();
+        });
+
+        e_flp_cart_click.setOnClickListener(v -> {
+            onCartClick();
+        });
+
+        e_csp_cart_click.setOnClickListener(v -> {
+            onCartClick();
+        });
+
+        e_mdp_cart_click.setOnClickListener(v -> {
+            onCartClick();
+        });
+
+        e_pgp_cart_click.setOnClickListener(v -> {
+            onCartClick();
+        });
+
+        e_vjmp_cart_click.setOnClickListener(v -> {
+            onCartClick();
+        });
+
+        e_bhp_cart_click.setOnClickListener(v -> {
+            onCartClick();
+        });
+
     }
+
+    public void RedirectProduct(int imageResId,String name, int price, String description){
+        Intent intent = new Intent(getApplicationContext(), product_explore.class);
+        intent.putExtra("img", imageResId);
+        intent.putExtra("name", name);
+        intent.putExtra("price", price);
+        intent.putExtra("description",description);
+        startActivity(intent);
+    }
+
+    public void onCartClick() {
+        Intent intent = new Intent(getApplicationContext(), cart.class);
+        startActivity(intent);
+    }
+
 }
